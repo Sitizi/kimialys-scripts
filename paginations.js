756321cb34869b9227c461c9b81ff6163dba2e8f
Dantin <dantin@iMac-de-Dantin.local>
2023-11-13 16:02:50 +0100

    [func] change pagination system

--- paginations.js
+++ paginations.js
@@ -37,8 +37,9 @@ nextBtn.on("click", () => {
 });
 
 btnsFilter.on("click", (e) => {
-    const filterName = e.target.innerText;
-    if (!filterName.length) return;
+    // get value of attribute data-filter
+    const filterName = e.target.getAttribute("data-filter");
+    if (!filterName || !filterName.length) return;
     articles.hide();
     articles = $(articlesSelector).filter(`[data-filter="${filterName}"]`);
     // show with fade effect
@@ -57,7 +58,25 @@ clearBtn.on("click", () => {
 });
 
 function updateCounter() {
-    counter.text(currentPage + "/" + Math.ceil(articles.length / itemsPerPage));
+    const maxPages = Math.ceil(articles.length / itemsPerPage);
+    // clean pagination
+    counter.empty();
+    for (let i = 0; i < maxPages; i++) {
+        // add button to pagination
+        if ($(`.btn-page-${i + 1}`).length === 0) {
+            const btn = $(`<button class="btn-page btn-page-${i + 1} ${currentPage === i + 1 ? "active" : ""}">${i + 1}</button>`);
+
+            counter.append(btn);
+
+            btn.on("click", () => {
+                currentPage = i + 1;
+                articles.hide();
+                articles.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).fadeIn();
+                updateCounter();
+                scrollToTopArticles();
+            });
+        }
+    }
 }
 
 function scrollToTopArticles() {
@@ -65,6 +84,3 @@ function scrollToTopArticles() {
     //     scrollTop: $(".article-cols").offset().top
     // }, 1000);
 }
-
-
-console.log("articles -> ", articles);
